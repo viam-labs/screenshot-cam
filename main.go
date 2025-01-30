85bbeb20a05f223d181e467b5033a0204b132418
Abe Winter <abe-winter@users.noreply.github.com>
2025-01-30 17:09:44 -0500

    non-module dump entrypoint for testing

--- main.go
+++ main.go
@@ -1,17 +1,39 @@
 package main
 
 import (
-	"github.com/viam-labs/screenshot-cam/models"
 	"context"
+	"image/png"
+	"os"
+
+	"github.com/kbinani/screenshot"
+	"github.com/viam-labs/screenshot-cam/models"
+	"go.viam.com/rdk/components/camera"
 	"go.viam.com/rdk/logging"
 	"go.viam.com/rdk/module"
 	"go.viam.com/utils"
-	"go.viam.com/rdk/components/camera"
-
 )
 
 func main() {
-	utils.ContextualMain(mainWithArgs, module.NewLoggerFromArgs("screenshot-cam"))
+	logger := module.NewLoggerFromArgs("screenshot-cam")
+	if os.Args[1] == "dump" {
+		logger.Info("dumping a screenshot instead of starting module")
+		img, err := screenshot.CaptureDisplay(0)
+		if err != nil {
+			panic(err)
+		}
+		path := "screenshot.png"
+		f, err := os.Create(path)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+		if err := png.Encode(f, img); err != nil {
+			panic(err)
+		}
+		logger.Infof("wrote to %s", path)
+		return
+	}
+	utils.ContextualMain(mainWithArgs, logger)
 }
 
 func mainWithArgs(ctx context.Context, args []string, logger logging.Logger) error {
@@ -19,11 +41,10 @@ func mainWithArgs(ctx context.Context, args []string, logger logging.Logger) err
 	if err != nil {
 		return err
 	}
-	
+
 	if err = screenshotCam.AddModelFromRegistry(ctx, camera.API, models.Screenshot); err != nil {
 		return err
 	}
-
 
 	err = screenshotCam.Start(ctx)
 	defer screenshotCam.Close(ctx)
