9ed5c07095dbfffdf4962924cc9b46d708a159ad
Abe Winter <abe.winter@viam.com>
2025-02-19 16:34:24 -0500

    optionally capture N screenshots and time them in 'parent' mode

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"image/png"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/kbinani/screenshot"
 	"github.com/viam-labs/screenshot-cam/models"
@@ -18,16 +21,29 @@ func main() {
 	logger := module.NewLoggerFromArgs("screenshot-cam")
 	var arg string
 	var capturePath string
-	if len(os.Args) >= 3 {
+	if len(os.Args) >= 2 {
 		arg = os.Args[1]
+	}
+	if len(os.Args) >= 3 {
 		capturePath = os.Args[2]
 	}
 	switch arg {
 	case "parent":
 		// parent is a test mode for spawning a child proc directly from session 0 CLI. see README.md for instructions.
-		if err := subproc.SpawnSelf(" child " + capturePath); err != nil {
-			panic(err)
+		n := 1
+		if len(os.Args) >= 4 {
+			if nTries, err := strconv.Atoi(os.Args[3]); err == nil {
+				n = nTries
+			}
 		}
+		t0 := time.Now()
+		for range n {
+			if err := subproc.SpawnSelf(" child " + capturePath); err != nil {
+				panic(err)
+			}
+		}
+		delta := time.Now().Sub(t0)
+		fmt.Printf("captured %d screenshots in %s seconds, %f per second", n, (delta / time.Second).String(), float64(n)/float64(delta/time.Second))
 	case "child":
 		// child is the subprocess started in session 1 by a session 0 parent. it does the work.
 		logger.Debugf("dumping a screenshot instead of starting module")
